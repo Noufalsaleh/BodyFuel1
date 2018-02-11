28bf42c8dbba7a5828f7e306f21a22a812d3b702
Nouf Alsaleh <nouf@macbook-air-alkhas-b-nouf.local>
2018-02-12 00:23:49 +0300

    Initial Commit

---
+++ BodyFuel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BodyFuel
+//
+//  Created by Nouf Alsaleh on 26/05/1439 AH.
+//  Copyright © 1439 Nouf Alsaleh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
